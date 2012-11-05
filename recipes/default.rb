7e090ceebd4e9cf589f0ceec8f0e1b795d5c68ab
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-05 14:07:14 -0800

    Force sysctl configuration file towards the end of run list

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,9 +16,20 @@ sysctl_path = if(node[:sysctl][:conf_dir])
 
 if(sysctl_path)
   template sysctl_path do
+    action :nothing
     source 'sysctl.conf.erb'
     mode '0644'
     notifies :start, resources(:service => 'procps')
+    only_if do
+      node[:sysctl][:attributes] && !node[:sysctl][:attributes].empty?
+    end
+  end
+
+  ruby_block 'sysctl config notifier' do
+    block do
+      true
+    end
+    notifies :create, resources(:template => sysctl_path), :delayed
   end
 end
 
